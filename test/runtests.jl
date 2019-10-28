0975fd17c46bc84a58e7da036ca5a977a0dd27f7
Jose Daniel Lara <jdlara@berkeley.edu>
2019-10-28 14:55:36 -0700

    add MOI newer files

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,30 @@ using Xpress, Test
     @test Xpress.getprobname(xp) == ""
     @test Xpress.setprobname(xp, "xpress-optimization-problem") == nothing
     @test Xpress.getprobname(xp) == "xpress-optimization-problem"
+using SparseArrays
+using LinearAlgebra
+using Test
+
+#=
+tests = ["xprs_attrs_test",
+         "lp_01a",
+         "lp_01b",
+         "lp_02",
+         "lp_03",
+         "lp_04",
+         "mip_01",
+         "qp_01",
+         "qp_02",
+         "qcqp_01",
+         "iis",
+         "mathprog",
+         "MOIWrapper",
+        #  "wordhunt"
+         ]
+=#
+
+tests = ["MOI_Wrapper",
+        #  "wordhunt"
+         ]
 
 end
-
